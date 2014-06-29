3b73e220843ddfb875989e8b00a59556293be97a
Frank Woeckener <fwoeck@gmail.com>
2014-06-29 05:07:48 +0200

    basic setup

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'axlsx'
+gem 'mongoid'
+gem 'rufus-scheduler'
+
+platforms :rbx do
+  gem 'rubysl'
+end
+
+gem 'rubygems-bundler', require: nil
+gem 'git-smart',        require: nil
---
+++ daemon.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+STDOUT.sync = true
+STDERR.sync = true
+
+require 'bundler'
+Bundler.require
+
+require 'yaml'
+require 'json'
+require 'axlsx'
+require 'mongoid'
+require 'rufus-scheduler'
+
+PushConf = YAML.load(File.read(File.join('./config/app.yml')))
