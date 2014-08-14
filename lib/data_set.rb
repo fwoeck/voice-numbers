25802044d5e2f3a07785fe915436c4ac00df2f1f
Frank Wöckener <fwoeck@gmail.com>
2014-08-14 08:56:49 +0200

    add more call statistics

--- lib/data_set.rb
+++ lib/data_set.rb
@@ -1,4 +1,6 @@
 require 'json'
+require 'time'
+
 
 class DataSet
 
@@ -30,25 +32,52 @@ class DataSet
   end
 
 
+  def time_now
+    @memo_time_now ||= Time.now.utc
+  end
+
+
+  def queued_calls_delay_max
+    time_now - (call_queued_times.first || time_now)
+  end
+
+
+  def queued_calls_delay_avg
+    return 0 if call_queued_times.size == 0
+
+    (call_queued_times.inject(0) { |sum, t|
+      sum += time_now - t
+    } / call_queued_times.size).floor
+  end
+
+
   def to_json
     JSON.dump({
-      active_call_count:     active_call_count,
-      queued_call_count:     queued_call_count,
-      pre_queued_call_count: pre_queued_call_count,
-      dispatched_call_count: dispatched_call_count
+      active_call_count:      active_call_count,
+      queued_call_count:      queued_call_count,
+      pre_queued_call_count:  pre_queued_call_count,
+      dispatched_call_count:  dispatched_call_count,
+      queued_calls_delay_max: queued_calls_delay_max,
+      queued_calls_delay_avg: queued_calls_delay_avg
     })
   end
 
 
   private
 
+
+  def call_queued_times
+    @memo_delays ||= queued_calls.map { |c| Time.parse c['QueuedAt'] }.sort
+  end
+
+
   def pre_queued_calls
-    incoming_calls.select { |c| !c['Skill'] && !c['DispatchedAt'] }
+    incoming_calls.select { |c| !c['QueuedAt'] && !c['DispatchedAt'] }
   end
 
 
   def queued_calls
-    incoming_calls.select { |c| c['Skill'] && !c['DispatchedAt'] }
+    incoming_calls.select { |c| c['QueuedAt'] && !c['DispatchedAt'] }
   end
 
 
