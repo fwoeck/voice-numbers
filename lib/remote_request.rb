717a531c7b7c3c908417375c4fd40b0fb7134fd9
Frank Wöckener <fwoeck@gmail.com>
2014-09-18 07:32:16 +0200

    rename handle_update to handle_message

--- lib/remote_request.rb
+++ lib/remote_request.rb
@@ -3,7 +3,7 @@ class RemoteRequest
   attr_accessor :id, :verb, :klass, :params, :req_from, :res_to, :value, :status, :error
 
 
-  def handle_update
+  def handle_message
     Celluloid::Actor[:rpc].async.perform_rpc_request(self)
   end
 end
