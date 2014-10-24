5be6acc3d745f753cd8d0bf6ce18cfb7211f25b2
Frank Wöckener <fwoeck@gmail.com>
2014-10-24 17:23:20 +0000

    cleanup scheduler

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -3,6 +3,14 @@ RS = Rufus::Scheduler.new
 module Scheduler
 
   def self.start
+    prepare_shutdown
+    schedule_reports
+    schedule_dataset
+    join_threads
+  end
+
+
+  def self.prepare_shutdown
     Signal.trap('TERM') do
       puts "#{Time.now.utc} :: Shutting down.."
       sleep 1 while RS.running_jobs.size > 0
@@ -12,8 +20,10 @@ module Scheduler
       puts "#{Time.now.utc} :: Numbers finished.."
       exit
     end
+  end
 
 
+  def self.schedule_reports
     RS.cron '* * * * *', overlap: false do
       GeneratesReports.new(
         AggregatesNumbers.new Numbers.set_timestamp
@@ -21,20 +31,23 @@ module Scheduler
 
       RrdTool.render_images
     end
+  end
 
 
+  def self.schedule_dataset
     RS.every '2s' do
       ds = Dataset.new(Call.all)
       ds.store
+
       RrdTool.update_with(ds)
     end
+  end
 
 
+  def self.join_threads
     puts "#{Time.now.utc} :: Numbers launched.."
-    begin
-      RS.join
-    rescue ThreadError
-      # see https://github.com/jmettraux/rufus-scheduler/issues/98
-    end
+    RS.join
+  rescue ThreadError
+    # see https://github.com/jmettraux/rufus-scheduler/issues/98
   end
 end
